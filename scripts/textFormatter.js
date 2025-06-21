5956baf422e49d76f79ca4fc3cde72490f118a2f
WiViW <wiviw.public@gmail.com>
2025-06-20 19:07:24 -0500

    Added textFormatter.js & implemented a feature to cleanup dirty copies of text to be pasted more nicely.

---
+++ scripts/textFormatter.js
@@ -0,0 +1,20 @@
+const inputField = document.getElementById('inputField');
+const outputdiv = document.getElementById('outputdiv');
+
+function copyDiv(){
+  outputdiv.innerHTML = this.value.replaceAll("\n", "<br>");
+  navigator.clipboard
+    .write([
+      new ClipboardItem({
+        "text/html": new Blob([outputdiv.innerHTML], {
+          type: "text/html",
+        }),
+        "text/plain": new Blob([outputdiv.innerHTML], {
+          type: "text/plain",
+        }),
+      }),
+    ])
+    .then(() => console.log("Copied"))
+    .catch((e) => console.log(e));
+}
+inputField.addEventListener('change', updateForm);
